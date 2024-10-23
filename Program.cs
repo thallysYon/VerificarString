03880cfffc164d5d6698d0e30748d82168543bbd
THALLYS FEITOSA <thallys.feitosa@fatec.sp.gov.br>
2024-10-23 15:51:41 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿int contadorMaiusculas = 0;
+int contadorMinusculas = 0;
+string palavra;
+
+Console.WriteLine("Digite uma string:");
+palavra = Console.ReadLine();
+
+foreach (char p in palavra)
+{
+    if (p == 'A')
+    {
+        contadorMaiusculas++;
+    }
+    if(p == 'a')
+    {
+        contadorMinusculas++;
+    }
+}
+
+if (contadorMaiusculas > 0 || contadorMinusculas > 0)
+{
+    Console.WriteLine($"A letra 'a' apareceu. Sendo {contadorMaiusculas} vezes como maiúscilas e {contadorMinusculas} como minúsculas.");
+}
+else
+{
+    Console.WriteLine("A letra 'a' não foi encontrada na string.");
+}
